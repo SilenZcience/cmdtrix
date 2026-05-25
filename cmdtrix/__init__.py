5aed5514fbe9053c546c0cfcbcf12add5c5730a1
Silas Kraume <silas.kraume1552@gmail.com>
2026-05-25 16:48:10 +0200

    bump version

--- cmdtrix/__init__.py
+++ cmdtrix/__init__.py
@@ -1,5 +1,5 @@
 """Matrix-console-effect made in Python."""
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 __sysversion__ = '3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)]'
 __author__ = 'Silas A. Kraume'
